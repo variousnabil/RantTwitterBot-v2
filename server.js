cd0d962b414e4d0e3f94451bd5a1928ceda47bcb
variousnabil <varionabilr@gmail.com>
2020-08-16 22:11:20 +0700

    remove useless code

--- server.js
+++ server.js
@@ -13,30 +13,28 @@ var T = new Twit({
 
 let all_bangsat_kau = [];
 
-(() => {
-    // load the file
-    var lineReader = require('readline').createInterface({
-        input: require('fs').createReadStream('all_bangsat_kau.txt')
-    });
+// load the file
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('all_bangsat_kau.txt')
+});
 
-    // store every line into an array
-    lineReader.on('line', function (line) {
-        all_bangsat_kau.push(line);
-    });
+// store every line of tweets into an array
+lineReader.on('line', function (line) {
+    all_bangsat_kau.push(line);
+});
 
-    // when every line already stored in array, send every tweet randomly every 1 minutes (worker)
-    lineReader.on('close', () => {
-        setInterval(() => {
-            const randomIndex = Math.floor(Math.random() * all_bangsat_kau.length);
-            const status = all_bangsat_kau[randomIndex];
-            T.post('statuses/update', { status }, function (err, data, response) {
-                if (err) return console.log(err);
-                console.log('tweet sent:', data.text);
-                console.log('sleeping for 1 day...');
-            });
-        }, 24 * 60 * 60 * 1000);
-    });
-})();
+// when every line already stored in array, send every tweet randomly every 1 minutes (worker)
+lineReader.on('close', () => {
+    setInterval(() => {
+        const randomIndex = Math.floor(Math.random() * all_bangsat_kau.length);
+        const status = all_bangsat_kau[randomIndex];
+        T.post('statuses/update', { status }, function (err, data, response) {
+            if (err) return console.log(err);
+            console.log('tweet sent:', data.text);
+            console.log('sleeping for 1 day...');
+        });
+    }, 24 * 60 * 60 * 1000);
+});
 
 
 
